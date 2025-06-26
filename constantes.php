b7826ab7a601561d5473fa6f0ec13ec15845e0c5
WallaceRCarvalho <wacenacapa@gmail.com>
2025-06-25 21:08:27 -0300

    adicionar constantes no curso php

---
+++ constantes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cursophp-constantes</title>
+    </head>
+    <body>
+        <h1>Constantes</h1>
+    <?php
+
+    //estudos de constantes em php
+    //define(nome_constante, valor_constante[, igone_caso]
+
+        define("constante1", "Wallace");
+        define("constante2", "Wallace", true);
+        define("constante3", 52);
+        echo constante1 . "<br>";
+        echo constante2 . "<br>";
+        echo constante3 . "<br>";
+        
+        define('LINGUAGENS', array('C','JAVA','PYTHON'));
+        echo LINGUAGENS[2]; // Saida: "PYTHON"
+    ?>
+</body>
+</html>
+
